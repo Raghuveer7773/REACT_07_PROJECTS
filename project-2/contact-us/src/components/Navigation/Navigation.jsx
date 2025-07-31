988bdcd4d07ae43bb630a523306afa9f69126933
Raghuvver <raghuveersinghrathore245@gmail.com>
2025-07-31 16:43:59 +0530

    Navigation

---
+++ project-2/contact-us/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import styles from './Navigation.module.css'
+const Navigation = () => {
+    console.log(styles);
+    return <nav className= {`${styles.navigation} container`}>
+        <div className="logo">
+            <img src="public/images/Frame 2 1.png" alt="" />
+        </div>
+
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
+};
+
+
+export default Navigation;
